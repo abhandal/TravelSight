83591f7051a3df0e9ea3d035714d61043e766a29
Pirasath Vallipuram <pirasathv@gmail.com>
2016-01-23 19:40:23 -0500

    yolocommit

--- web/js/functions.js
+++ web/js/functions.js
@@ -11,6 +11,31 @@ $(document).ready(function() {
 	$.getJSON("https://raw.githubusercontent.com/pirasathv/TravelSight/master/data/flight_data_lasvegas.json").then(function(response) {
 		console.log(response);
 		flight_data_lasvegas = response;
+	
+	for (key in flight_data_lasvegas) {
+
+		delay(flight_data_lasvegas[key]);	
+	  //console.log(flight_data_lasvegas[key].carrier_name);
+	}
+	
 	});
 
 });
+
+function delay(airportDelay){
+	console.log(airportDelay);
+	var divisor = airportDelay.arr_flights;
+	var arr_del15 = (airportDelay.arr_del15/divisor)*100;
+	var carrier_ct = (airportDelay.carrier_ct/divisor)*100;
+	var weather_ct = (airportDelay.weather_ct/divisor)*100;
+	var nas_ct = (airportDelay.nas_ct/divisor)*100;
+	var security_ct = (airportDelay.security_ct/divisor)*100;
+	var late_aircraft_ct = (airportDelay.late_aircraft_ct/divisor)*100;
+	var arr_cancelled = (airportDelay.arr_cancelled/divisor)*100;
+	var arr_diverted = (airportDelay.arr_diverted/divisor)*100;
+
+	var total_delay = arr_del15 + carrier_ct + weather_ct + nas_ct + security_ct + late_aircraft_ct + arr_cancelled + arr_diverted;
+
+	console.log(total_delay);
+}
+
